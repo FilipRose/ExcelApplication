0eb3a2bad0fd11122ac24210afb06118b6c29086
Filip Rosicki <filip21rosicki@gmail.com>
2022-05-25 17:32:16 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ExcelApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(613, 400);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(193, 23);
+            this.createBtn.TabIndex = 0;
+            this.createBtn.Text = "Create Excel Raport";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(441, 400);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(166, 23);
+            this.convertBtn.TabIndex = 1;
+            this.convertBtn.Text = "Import xml file";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(613, 429);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(193, 23);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "Open Excel File";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(781, 344);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(64, 22);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(257, 20);
+            this.txtSearch.TabIndex = 12;
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(327, 19);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(716, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Total records ???";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 526);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.createBtn);
+            this.Name = "Form1";
+            this.Text = "Excel Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ExcelApplication
+{
+    public partial class Form1 : Form
+
+    {
+        DataSet ds = new DataSet();
+        DataTable datat = new DataTable();
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog2 = new OpenFileDialog();
+                openFileDialog2.InitialDirectory = "c:\\";
+                openFileDialog2.Filter = "Excel files (*.xlsx)|*.xlsx| Excel 2007 (*.xls)| *.xls";
+                openFileDialog2.FilterIndex = 1;
+
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    DataTable datat = Excel.DataGridView_To_Datatable(dataGridView1);
+                    datat.exportToExcel(openFileDialog2.FileName);
+                    MessageBox.Show("Data is exported");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+        
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+          
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Select file";
+            openFileDialog1.Filter = "All files (*.*)|*_*|XML File (*.xml)|*.xml";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.ShowDialog();
+            if(openFileDialog1.FileName != " ")
+            {
+                ds.ReadXml(openFileDialog1.FileName);
+                dataGridView1.DataSource = ds.Tables[0];
+                MessageBox.Show("XML Data Imported");
+            }
+        }
+
+
+
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|* .xlsx", Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    DataTable dt = new DataTable();
+                    using (XLWorkbook wb = new XLWorkbook(ofd.FileName))
+                    {
+
+                        bool isFirstRow = true;
+                        var rows = wb.Worksheet(1).RowsUsed();
+                        foreach (var row in rows)
+                        {
+                            if (isFirstRow)
+                            {
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Columns.Add(cell.Value.ToString());
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+                            }
+                        }
+                        dataGridView1.DataSource = dt.DefaultView;
+                        label3.Text = $"Total records: {dataGridView1.RowCount}";
+                        Cursor.Current = Cursors.Default;
+                    }
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = dataGridView1.DataSource as DataView;
+                if (dv != null)
+                    dv.RowFilter = txtSearch.Text;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                btnSearch.PerformClick();
+           
+        }
+    }
+}
